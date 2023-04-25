c61ccc44221d0540ed305739d25cd763c299f837
Phillip Cunliffe <phillip.cunliffe@gmail.com>
2023-04-24 20:55:31 -0700

    Adding custom agent

--- src/getChain.ts
+++ src/getChain.ts
@@ -1,12 +1,11 @@
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAI } from "langchain/llms/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { VectorDBQAChain } from "langchain/chains";
 
-async function getChain() {
+async function getChain(model) {
   const loadedVectorStore = await HNSWLib.load('./vectors', new OpenAIEmbeddings());
-  const model = new OpenAI({});
-  return ConversationalRetrievalQAChain.fromLLM(model, loadedVectorStore.asRetriever())
+  return VectorDBQAChain.fromLLM(model, loadedVectorStore)
 }
 
 export { getChain };
